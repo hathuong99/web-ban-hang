fcc7097afba9fbb89e6f2ce536a1ece27a3adf36
ha-thuong99 <ha-thuong99>
2022-08-27 22:37:50 +0700

    create project

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf8" />
+  <title>Thuongmeo Mobile Shop - Đăng nhập hệ thống</title>
+  <link rel="stylesheet" type="text/css" href="dangnhap.css" />
+</head>
+
+<body>
+
+  <form method="post">
+    <div id="form-login">
+      <h2>đăng nhập hệ thống quản trị</h2>
+      <center><span style="color:red;"></span></center>
+      <ul>
+        <li><label>tài khoản</label><input type="text" name="tk" /></li>
+        <li><label>mật khẩu</label><input type="password" name="mk" /></li>
+        <li><label>ghi nhớ</label><input type="checkbox" name="check" checked="checked" /></li>
+        <li><input type="submit" name="submit" value="Đăng nhập" /> <input type="reset" name="resset" value="Làm mới" />
+        </li>
+      </ul>
+    </div>
+  </form>
+
+</body>
+
+</html>
